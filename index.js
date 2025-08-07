bb05d0fce5f648dbf5b50c278b810c616bda1d71
Miro Paananen <henrikdonatus07@outlook.com>
2025-08-07 09:22:22 +0300

    toimiiko

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+      /*while (true) {
+        setTimeout(fetchStatus, 5000)
+      }*/
+
       setInterval(fetchStatus, 1000);
+      
       async function fetchStatus() {
         try {
           const response = await fetch('https://api.lanyard.rest/v1/users/491473057538506763');
@@ -62,7 +67,7 @@
                     const rgb = vibrant.rgb;
                     const rgbString = `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
 
-                    document.getElementById("spotifyStatus").style.borderColor = rgbString;
+                    document.getElementById("spotifyStatus").style.backgroundColor = rgbString;
                   }
                 })
                 .catch(err => {
@@ -72,6 +77,7 @@
 
             document.getElementById("spotifyArt").style.marginRight = "10px";
             document.getElementById("spotifyArt").style.marginLeft = "5px";
+            document.getElementById("spotifyArt").style.borderStyle = 'solid';
             document.getElementById("trackLink").href = `https://open.spotify.com/track/${spotify.track_id}`;
             /* // Album Checks 
             if (spotify.album === spotify.artist) {
@@ -85,6 +91,7 @@
             document.getElementById("spotifyTrack").innerHTML = spotify.song + ' |';
             document.getElementById("spotifyArtist").innerHTML = spotify.artist;
             document.getElementById("progress-container").style.borderStyle = 'solid';
+            document.getElementById("progress-container").style.boxShadow = '0px 0px 5px black';
 
             const spotifyStart = spotify.timestamps.start;
             const spotifyEnd = spotify.timestamps.end;
@@ -110,6 +117,8 @@
               document.getElementById("progress-bar").style.width = `${percent}%`;
             }
           } else {
+            document.getElementById("spotifyStatus").style.backgroundColor = null;
+            document.getElementById("spotifyArt").style.borderStyle = 'none';
             document.getElementById("spotifyArt").src = "transparent-placeholder.png";
             document.getElementById("spotifyArt").style.marginLeft = "8px";
             document.getElementById("spotifyArt").style.marginRight = "0px";
@@ -117,6 +126,7 @@
             document.getElementById("spotifyArtist").innerHTML = '';
             document.getElementById("trackLink").href = "";
             document.getElementById("progress-container").style.borderStyle = 'none';
+            document.getElementById("progress-container").style.boxShadow = 'none';
             document.getElementById("progress-bar").style.width = `0%`;
             document.getElementById("songDuration").innerHTML = ``
 
@@ -135,8 +145,6 @@
           document.getElementById("lightModeButton").innerHTML = "🌙";
           document.getElementById("discordStatus").style.color = "black";
           document.getElementById("discordStatus").style.borderColor = "black";
-          document.getElementById("progress-container").style.borderColor = "black";
-          document.getElementById("progress-bar").style.backgroundColor = "black";
           if (document.getElementById("discordStatus").style.backgroundColor === "black") {
             document.getElementById("discordStatus").style.backgroundColor = "white";
           }
@@ -146,8 +154,6 @@
           document.getElementById("lightModeButton").innerHTML = "☀️";
           document.getElementById("discordStatus").style.color = "white";
           document.getElementById("discordStatus").style.borderColor = "white";
-          document.getElementById("progress-container").style.borderColor = "white";
-          document.getElementById("progress-bar").style.backgroundColor = "white";
           if (document.getElementById("discordStatus").style.backgroundColor === "white") {
             document.getElementById("discordStatus").style.backgroundColor = "black";
           }
